f64eb23af8eb027565a48587e2b10fc18a37d5b9
JefersonYandun <jeferson.yandun237@ist17dejulio.edu.ec>
2023-11-13 13:13:01 -0500

    validaciones

--- app/src/main/java/com/example/primera_aplicacion/AppNueva.java
+++ app/src/main/java/com/example/primera_aplicacion/AppNueva.java
@@ -2,7 +2,12 @@ package com.example.primera_aplicacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AppNueva extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class AppNueva extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_nueva);
+
+        //CREAR OBJETOS DEL MISMO TIPO DEL LAYOUT
+        EditText edNombres,edAepllios,edCedula,edCorreo,edDireccion;
+        Button btnvalidar=findViewById(R.id.btnvalidar);
+
+        edNombres=findViewById(R.id.txtnombres);
+        edAepllios=findViewById(R.id.txtapellidos);
+        edCedula=findViewById(R.id.txtcedula);
+        edCorreo=findViewById(R.id.txtcorreo);
+        edDireccion=findViewById(R.id.txtdireccion);
+
+        btnvalidar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre=edNombres.getText().toString();
+                if(!nombre.isEmpty()){
+
+
+                   Toast.makeText(AppNueva.this,"Hola gei", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
     }
 }
